33d46e9827bda627179858d0b792b796738ad57b
Kasif <your-kasifoutlook2@gmail.com>
2026-03-27 02:57:06 +0530

    final frontend ready for deployment

--- zamzam-backend/server.js
+++ zamzam-backend/server.js
@@ -10,6 +10,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
+app.use(express.static(__dirname));
 // =====================
 // ✅ ENSURE UPLOAD FOLDER EXISTS
 // =====================
